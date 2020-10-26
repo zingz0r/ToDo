a86c70fee92d057ae44eefd1727501e87398978e
zingz0r <zingz0r@tothnet.hu>
2020-10-26 20:23:36 +0100

    entity and map created

--- src/ToDo.Persistence/Entities/ToDoEntity.cs
+++ src/ToDo.Persistence/Entities/ToDoEntity.cs
@@ -1,6 +1,11 @@
-﻿namespace ToDo.Persistence.Entities
+﻿using System;
+
+namespace ToDo.Persistence.Entities
 {
     public class ToDoEntity
     {
+        public virtual Guid Id { get; set; }
+        public virtual string Task { get; set; }
+        public virtual bool IsFinished { get; set; }
     }
 }
--- src/ToDo.Persistence/Maps/ToDoEntityMap.cs
+++ src/ToDo.Persistence/Maps/ToDoEntityMap.cs
@@ -5,6 +5,13 @@ namespace ToDo.Persistence.Maps
 {
     public class ToDoEntityMap : ClassMap<ToDoEntity>
     {
+        public ToDoEntityMap()
+        {
+            Table("todo");
 
+            Id(x => x.Id).GeneratedBy.Assigned();
+            Map(x => x.IsFinished);
+            Map(x => x.Task);
+        }
     }
 }
