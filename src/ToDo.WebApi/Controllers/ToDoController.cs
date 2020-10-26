32dcb3093d8f6d875e17fa2397e6c82bd7dfeeab
zingz0r <zingz0r@tothnet.hu>
2020-10-26 20:41:12 +0100

    add todo entity to db

--- src/ToDo.WebApi/Controllers/ToDoController.cs
+++ src/ToDo.WebApi/Controllers/ToDoController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Serilog;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ToDo.Application.Models;
+using ToDo.Persistence.Entities;
+using ToDo.Persistence.Repositories;
 
 namespace ToDo.WebApi.Controllers
 {
@@ -7,11 +13,30 @@ namespace ToDo.WebApi.Controllers
     [ApiController]
     public class ToDoController : ControllerBase
     {
+        private readonly IRepository<ToDoEntity> _toDoRepository;
         private readonly ILogger _logger;
 
-        public ToDoController(ILogger logger)
+        public ToDoController(ILogger logger, IRepository<ToDoEntity> toDoRepository)
         {
+            _toDoRepository = toDoRepository;
             _logger = logger.ForContext<ToDoController>();
+        }
+
+        [HttpPost]
+        public async Task AddAsync([FromBody] AddToDoModel todoModel, CancellationToken cancellationToken)
+        {
+            await _toDoRepository.ExecuteAsync(async () =>
+            {
+                var entity = new ToDoEntity
+                {
+                    Id = Guid.NewGuid(),
+                    IsFinished = false,
+                    Task = todoModel.Task
+                };
+
+                _logger.Information("Adding following item to database: {@item}", entity);
+                await _toDoRepository.AddAsync(entity, cancellationToken).ConfigureAwait(false);
+            }, cancellationToken).ConfigureAwait(false);
         }
     }
 }
