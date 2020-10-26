37aef44cebaf72d02913a2cfd4ae75025809c30d
zingz0r <zingz0r@tothnet.hu>
2020-10-26 19:31:31 +0100

    removed unnecessary second ToDo.WebApi directory

---
+++ src/ToDo.WebApi/Controllers/ToDoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace ToDo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public ToDoController(ILogger logger)
+        {
+            _logger = logger.ForContext<ToDoController>();
+        }
+    }
+}
