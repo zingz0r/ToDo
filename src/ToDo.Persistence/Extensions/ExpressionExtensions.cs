9f9d661e2e01d6997d5ea008a94da7881fe3fd6e
zingz0r <zingz0r@tothnet.hu>
2020-10-26 21:10:09 +0100

    extension for pretty predicate in log

---
+++ src/ToDo.Persistence/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace ToDo.Persistence.Extensions
+{
+    public static class ExpressionExtensions
+    {
+        public static string ToReadableString(this Expression expression)
+        {
+            var regex = new Regex(@"value\(\S*\)\.");
+            return regex.Replace(expression.ToString(), "");
+        }
+    }
+}
