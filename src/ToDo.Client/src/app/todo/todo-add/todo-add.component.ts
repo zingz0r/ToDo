ebddbf2727b65662721b8d5551da36c2bd17b010
zingz0r <zingz0r@tothnet.hu>
2020-10-27 22:56:41 +0100

    todo add component

---
+++ src/ToDo.Client/src/app/todo/todo-add/todo-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-add',
+  templateUrl: './todo-add.component.html',
+  styleUrls: ['./todo-add.component.scss']
+})
+export class ToDoAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
