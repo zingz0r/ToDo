3a18f259b1fe593c7e7fb2f8c21eadc2e7697ddd
zingz0r <zingz0r@tothnet.hu>
2020-10-27 22:55:57 +0100

    search query created

---
+++ src/ToDo.Client/src/app/models/search.model.ts
@@ -0,0 +1,4 @@
+export class SearchModel {
+    pattern: string;
+    state: string;
+}
--- src/ToDo.Client/src/app/services/todo.service.ts
+++ src/ToDo.Client/src/app/services/todo.service.ts
@@ -1,22 +1,25 @@
+import { debounce } from 'rxjs/internal/operators/debounce';
 import { environment } from 'src/environments/environment';
+import { timer } from 'rxjs';
 import { AlertService } from './alert.service';
-import { Endpoint } from '../../common/endpoints';
+import { Endpoint } from 'src/common/endpoints';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ToDoState } from '../../common/todostate';
+import { ToDoModel } from 'src/app/models/todo.model';
+import { ToDoState } from 'src/common/todostate';
 
 @Injectable({
     providedIn: 'root'
 })
 
-export class BetService {
+export class ToDoService {
     private baseUrl = environment.settings.backend;
     constructor(
         private http: HttpClient,
         private alertService: AlertService
     ) { }
 
-    public Search(pattern: string, state: ToDoState, then: (res) => void): void {
+    public Search(pattern: string, state: ToDoState, then: (res: ToDoModel[]) => void): void {
         this.http
             .get<any>(`${this.baseUrl}/${Endpoint.ToDo.Base}/${Endpoint.ToDo.Search}/${pattern}/${state}`)
             .toPromise()
