2352a3689ff087f0d114f3c9bcb653a8fedd113a
zingz0r <zingz0r@tothnet.hu>
2020-10-27 22:56:53 +0100

    todo base component

--- src/ToDo.Client/src/app/todo/todo.component.ts
+++ src/ToDo.Client/src/app/todo/todo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchModel } from 'src/app/models/search.model';
+import { ToDoService } from 'src/app/services/todo.service';
+import { ToDoState } from 'src/common/todostate';
+import { ToDoModel } from '../models/todo.model';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private todoService: ToDoService) { 
+    this.todoService.Search('*', ToDoState.Any, (res: ToDoModel[]) => {
+      this.toDoItems = res;
+     });
+  }
+
+  toDoItems: ToDoModel[];
 
   ngOnInit(): void {
   }
 
+  onSearch(event: SearchModel): void {
+    this.todoService.Search(event.pattern, event.state, (res: ToDoModel[]) => {
+      this.toDoItems = res;
+     });
+  }
 }
