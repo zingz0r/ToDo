6e82c33b831a518e4db0cb79cdd5308a7e1d1850
zingz0r <zingz0r@tothnet.hu>
2020-10-27 22:56:30 +0100

    todo search component

---
+++ src/ToDo.Client/src/app/todo/todo-search/todo-search.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Output, EventEmitter } from '@angular/core';
+import { SearchModel } from 'src/app/models/search.model';
+import { ToDoState } from 'src/common/todostate';
+
+interface State {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-todo-search',
+  templateUrl: './todo-search.component.html',
+  styleUrls: ['./todo-search.component.scss']
+})
+export class ToDoSearchComponent implements OnInit {
+
+  constructor() { }
+
+  @Output() searchEvent = new EventEmitter<SearchModel>();
+
+  selectedState = ToDoState.Any;
+
+  todoStates: State[] = [
+    {value: ToDoState.Any, viewValue: ToDoState.Any},
+    {value: ToDoState.Finished, viewValue: ToDoState.Finished},
+    {value: ToDoState.Ongoing, viewValue: ToDoState.Ongoing}
+  ];
+
+  searchForm = new FormGroup({
+    pattern: new FormControl(),
+    state: new FormControl(),
+  });
+
+  ngOnInit(): void {
+  }
+
+  onSearch(): void {
+    this.searchEvent.emit({pattern: this.searchForm.value.pattern ? this.searchForm.value.pattern : '*', state: this.selectedState});
+  }
+
+}
