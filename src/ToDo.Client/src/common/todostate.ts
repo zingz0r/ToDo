4fb353ac673ad358b324f1de29d8f6b723c9db8a
zingz0r <zingz0r@tothnet.hu>
2020-10-27 22:54:56 +0100

    todo states

--- src/ToDo.Client/src/common/todostate.ts
+++ src/ToDo.Client/src/common/todostate.ts
@@ -1,5 +1,5 @@
-export enum ToDoState {
-    Any,
-    Finished,
-    OnGoing
+export class ToDoState {
+    public static readonly Any = 'Any';
+    public static readonly Finished = 'Finished';
+    public static readonly Ongoing = 'Ongoing';
 }
